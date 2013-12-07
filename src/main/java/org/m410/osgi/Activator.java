0d85ca1bef27447bcacd1c481c25e6bcda3eae8b
Michael Fortin <michael@m410.us>
2013-12-06 23:50:27 -0500

    add source and javadoc to poms

--- src/main/java/org/m410/osgi/Activator.java
+++ src/main/java/org/m410/osgi/Activator.java
@@ -4,7 +4,9 @@ import org.osgi.framework.*;
 import org.m410.osgi.service.HelloService;
 
 /**
- * And to read this serice:
+ * And to read this service:
+ *
+ * <pre>
  * public class Activator implements BundleActivator {
  * ServiceReference helloServiceReference;
  * public void start(BundleContext context) throws Exception {
@@ -12,13 +14,13 @@ import org.m410.osgi.service.HelloService;
  * helloServiceReference= context.getServiceReference(HelloService.class.getName());
  * HelloService helloService =(HelloService)context.getService(helloServiceReference);
  * System.out.println(helloService.sayHello());
- * <p/>
  * }
  * public void stop(BundleContext context) throws Exception {
  * System.out.println("Goodbye World!!");
  * context.ungetService(helloServiceReference);
  * }
  * }
+ * </pre>
  */
 public class Activator implements BundleActivator {
 
