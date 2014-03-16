f3d531212bafc41cb2e131a75ba50178d157a2ab
Michael Fortin <michael@m410.us>
2014-03-16 10:43:23 -0400

    init

---
+++ src/main/java/org/m410/fab/Activator.java
@@ -0,0 +1,41 @@
+package org.m410.fab;
+
+import org.osgi.framework.*;
+import org.m410.fab.service.HelloService;
+
+/**
+ * And to read this service:
+ *
+ * <pre>
+ * public class Activator implements BundleActivator {
+ * ServiceReference helloServiceReference;
+ * public void start(BundleContext context) throws Exception {
+ * System.out.println("Hello World!!");
+ * helloServiceReference= context.getServiceReference(HelloService.class.getName());
+ * HelloService helloService =(HelloService)context.getService(helloServiceReference);
+ * System.out.println(helloService.sayHello());
+ * }
+ * public void stop(BundleContext context) throws Exception {
+ * System.out.println("Goodbye World!!");
+ * context.ungetService(helloServiceReference);
+ * }
+ * }
+ * </pre>
+ */
+public class Activator implements BundleActivator {
+
+    ServiceReference helloServiceReference;
+
+    public void start(BundleContext context) throws Exception {
+        System.out.println("Hello World!!");
+        helloServiceReference= context.getServiceReference(HelloService.class.getName());
+        System.out.println("made service");
+        HelloService helloService =(HelloService)context.getService(helloServiceReference);
+        System.out.println("call service:" + helloService.sayHello());
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("Goodbye World!!");
+        context.ungetService(helloServiceReference);
+    }
+}
---
+++ src/main/java/org/m410/fab/service/HelloService.java
@@ -0,0 +1,10 @@
+package org.m410.fab.service;
+
+/**
+ * Document Me..
+ *
+ * @author Michael Fortin
+ */
+public interface HelloService {
+    String sayHello();
+}
