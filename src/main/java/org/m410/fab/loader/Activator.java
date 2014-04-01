da53704f8e611cd05835d337e03ec68c0d115426
Michael Fortin <michael@m410.us>
2014-04-01 09:56:57 -0400

    Cleanup init load, check for already loaded bundles

--- src/main/java/org/m410/fab/loader/Activator.java
+++ src/main/java/org/m410/fab/loader/Activator.java
@@ -4,9 +4,6 @@ import org.m410.fab.service.FabricateService;
 import org.m410.fab.service.FabricateServiceImpl;
 import org.osgi.framework.*;
 
-import java.io.File;
-import java.util.Arrays;
-
 /**
  * And to read this service:
  *
@@ -30,20 +27,11 @@ import java.util.Arrays;
 public class Activator implements BundleActivator {
     ServiceRegistration fabricateServiceRegistration;
 
+    @SuppressWarnings("unchecked")
     public void start(BundleContext context) throws Exception {
-        System.out.println("fab-loader-bundle");
         FabricateService fabricateService = new FabricateServiceImpl();
         final String name = FabricateService.class.getName();
         fabricateServiceRegistration = context.registerService(name, fabricateService, null);
-
-        String argsStr = System.getProperty("m410.cli.arguments");
-        String[] args = argsStr.substring(1,argsStr.length() -1).split("\\s*,\\s*");
-
-        FabricateService service = (FabricateService)context.getService(fabricateServiceRegistration.getReference());
-
-
-        service.modifyCommands();
-        service.execute(args);
     }
 
     public void stop(BundleContext context) throws Exception {
