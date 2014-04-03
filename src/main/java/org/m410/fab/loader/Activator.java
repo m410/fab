272867622a9da574f7e4238d4c25de2e48c36d94
Michael Fortin <michael@m410.us>
2014-04-02 22:13:59 -0400

    Added some mock tasks, check for bundle presence

--- src/main/java/org/m410/fab/loader/Activator.java
+++ src/main/java/org/m410/fab/loader/Activator.java
@@ -5,24 +5,8 @@ import org.m410.fab.service.FabricateServiceImpl;
 import org.osgi.framework.*;
 
 /**
- * And to read this service:
  *
- * <pre>
- * public class Activator implements BundleActivator {
- * ServiceReference helloServiceReference;
- * public void start(BundleContext context) throws Exception {
- * System.out.println("Hello World!!");
- * helloServiceReference= context.getServiceReference(HelloService.class.getName());
- * HelloService helloService =(HelloService)context.getService(helloServiceReference);
- * System.out.println(helloService.sayHello());
- * }
- * public void stop(BundleContext context) throws Exception {
- * System.out.println("Goodbye World!!");
- * context.ungetService(helloServiceReference);
- * }
- * }
- * </pre>
- *
+ * @author Michael Fortin
  */
 public class Activator implements BundleActivator {
     ServiceRegistration fabricateServiceRegistration;
