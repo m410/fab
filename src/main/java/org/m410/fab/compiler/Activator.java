3b1600ea68a0b9fff69fc0df5eb500817d6229ff
Michael Fortin <michael@m410.us>
2014-04-02 22:14:00 -0400

    Added some mock tasks, check for bundle presence

--- src/main/java/org/m410/fab/compiler/Activator.java
+++ src/main/java/org/m410/fab/compiler/Activator.java
@@ -1,29 +1,14 @@
 package org.m410.fab.compiler;
 
 import org.m410.fab.builder.Command;
+import org.m410.fab.builder.CommandModifier;
 import org.m410.fab.builder.Step;
 import org.m410.fab.service.FabricateService;
 import org.osgi.framework.*;
 
 
 /**
- * And to read this service:
- *
- * <pre>
- * public class Activator implements BundleActivator {
- * ServiceReference helloServiceReference;
- * public void start(BundleContext context) throws Exception {
- * System.out.println("Hello World!!");
- * helloServiceReference= context.getServiceReference(HelloService.class.getName());
- * HelloService helloService =(HelloService)context.getService(helloServiceReference);
- * System.out.println(helloService.sayHello());
- * }
- * public void stop(BundleContext context) throws Exception {
- * System.out.println("Goodbye World!!");
- * context.ungetService(helloServiceReference);
- * }
- * }
- * </pre>
+ * @author Michael Fortin
  */
 public class Activator implements BundleActivator {
 
@@ -32,13 +17,18 @@ public class Activator implements BundleActivator {
         ServiceReference fabricateServiceReference = context.getServiceReference(FabricateService.class.getName());
 
         FabricateService fabricateService =(FabricateService)context.getService(fabricateServiceReference);
-        fabricateService.addConfiguration("configuration2");
-        fabricateService.addCommand(
-                new Command("compile-src", "Compile project", false)
-                        .withStep(new Step("default")
-                                .withTask(new CompileTask())
-                        )
-        );
+
+        fabricateService.addCommandModifier(new CommandModifier() {
+            @Override public void modify(Command modifier) {
+                if(modifier.getName().equalsIgnoreCase("build")) {
+                    for (Step step : modifier.getSteps()) {
+                        if(step.getName().equalsIgnoreCase("compile"))
+                            step.append(new CompileTask());
+                    }
+                }
+            }
+        });
+
     }
 
     public void stop(BundleContext context) throws Exception {
