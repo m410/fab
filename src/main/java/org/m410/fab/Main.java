b05ebc7aac291d8ed5bfcd0f83ce807700bbd236
Michael Fortin <michael@m410.us>
2014-08-16 13:40:28 -0400

    Added new build tasks with inital tests

--- src/main/java/org/m410/fab/Main.java
+++ src/main/java/org/m410/fab/Main.java
@@ -15,6 +15,7 @@ import java.util.List;
  */
 public final class Main {
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Throwable {
         Options options = new Options();
         options.addOption("verbose", false, "Verbose output");
