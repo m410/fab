ff5ae3b9a120b2b4182b7d4eadf01ee1ef0643b4
Michael Fortin <mifortin@deliotte.com>
2014-08-23 14:55:10 -0400

    Added reflect

---
+++ src/main/java/org/m410/fab/garden/Reflect.java
@@ -0,0 +1,20 @@
+package org.m410.fab.garden;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * @author <a href="mailto:mifortin@deloitte.com">Michael Fortin</a>
+ */
+public class Reflect {
+    public static Class[] classTypeArgs(Object o) {
+        return ((Class[])((ParameterizedType)o.getClass().getGenericSuperclass())
+                .getActualTypeArguments());
+
+    }
+
+    public static Class[] methodTypeArgs(Method o) {
+        return o.getParameterTypes();
+
+    }
+}
