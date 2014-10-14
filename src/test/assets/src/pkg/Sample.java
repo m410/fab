e18975c55d59dcbfa6d03e377078be58ca6f4bd3
Michael Fortin <michael@m410.us>
2014-10-14 19:20:45 -0400

    Now builds

---
+++ src/test/assets/src/pkg/Sample.java
@@ -0,0 +1,15 @@
+package pkg;
+
+import java.lang.String;
+
+public final class Sample {
+    private String in;
+
+    public String getIn() {
+        return in;
+    }
+
+    public void setIn(String in) {
+        this.in = in;
+    }
+}
