47aa3b8657a85f7ed40285472791fb4bc665e8d8
Michael Fortin <michael@m410.us>
2014-10-14 19:20:46 -0400

    Now builds

--- src/main/java/org/m410/fab/javalib/Activator.java
+++ src/main/java/org/m410/fab/javalib/Activator.java
@@ -13,32 +13,7 @@ public class Activator implements BundleActivator {
     @SuppressWarnings("unchecked")
     public void start(BundleContext context) throws Exception {
         ServiceReference fabricateServiceReference = context.getServiceReference(FabricateService.class.getName());
-
         FabricateService fabricateService = (FabricateService) context.getService(fabricateServiceReference);
-        fabricateService.addCommandModifier(command -> {
-            if (command.getName().equalsIgnoreCase("build")) {
-                command.getSteps().stream()
-                        .filter(m -> m.getName().equals("compile"))
-                        .findFirst()
-                        .ifPresent(m -> m.append(new JavaCompileTask(JavaCompileTask.COMPILE_SRC)));
-                command.getSteps().stream()
-                        .filter(m -> m.getName().equals("test-compile"))
-                        .findFirst()
-                        .ifPresent(m -> m.append(new JavaCompileTask(JavaCompileTask.COMPILE_TEST)));
-                command.getSteps().stream()
-                        .filter(m -> m.getName().equals("test"))
-                        .findFirst()
-                        .ifPresent(m -> m.append(new JUnitTestRunnerTask()));
-                command.getSteps().stream()
-                        .filter(m -> m.getName().equals("initialize"))
-                        .findFirst()
-                        .ifPresent(m -> m.append(new IvyDependencyTask()));
-                command.getSteps().stream()
-                        .filter(m -> m.getName().equals("package"))
-                        .findFirst()
-                        .ifPresent(m -> m.append(new JarTask()));
-            }
-        });
     }
 
     public void stop(BundleContext context) throws Exception {
