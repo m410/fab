0f05664370d16725fb3120566f6c88cf4f451b3a
Michael Fortin <michael@m410.us>
2014-10-14 21:17:39 -0400

    Back to building, refactored code, moved to own projects

--- src/main/java/org/m410/fabricate/war/WarTask.java
+++ src/main/java/org/m410/fabricate/war/WarTask.java
@@ -1,7 +1,7 @@
 package org.m410.fabricate.war;
 
-import org.m410.fab.builder.BuildContext;
-import org.m410.fab.builder.Task;
+import org.m410.fabricate.builder.BuildContext;
+import org.m410.fabricate.builder.Task;
 
 import java.io.*;
 import java.nio.file.*;
