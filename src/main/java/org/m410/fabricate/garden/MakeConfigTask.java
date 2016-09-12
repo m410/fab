088855320aef001fcd757f66b6039cb730ed5521
Michael Fortin <michael@m410.us>
2016-09-12 13:57:44 -0400

    More tweaking to get running again

--- src/main/java/org/m410/fabricate/garden/MakeConfigTask.java
+++ src/main/java/org/m410/fabricate/garden/MakeConfigTask.java
@@ -1,19 +1,13 @@
 package org.m410.fabricate.garden;
 
 
-import org.m410.config.YamlConfig;
+import org.m410.config.YamlConfiguration;
 import org.m410.fabricate.builder.BuildContext;
 import org.m410.fabricate.builder.Task;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileWriter;
-import java.io.Writer;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author m410
@@ -31,6 +25,9 @@ public class MakeConfigTask implements Task {
         final String sourceOutputDir = context.getBuild().getSourceOutputDir();
         FileSystems.getDefault().getPath(sourceOutputDir).toFile().mkdirs();
         final Path path = FileSystems.getDefault().getPath(sourceOutputDir,"garden.fab.yml");
-        YamlConfig.write(context.getConfiguration(), path.toFile());
+
+        try (FileWriter writer = new FileWriter(path.toFile())) {
+            ((YamlConfiguration) context.getConfiguration()).write(writer);
+        }
     }
 }
