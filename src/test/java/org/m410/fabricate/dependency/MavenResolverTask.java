67eaa320c0db92bdc7b6fc134c07a53316668f80
Michael Fortin <michael@m410.us>
2016-09-13 21:10:16 -0400

    working demo app

--- src/test/java/org/m410/fabricate/dependency/MavenResolverTask.java
+++ src/test/java/org/m410/fabricate/dependency/MavenResolverTask.java
@@ -24,8 +24,17 @@ public class MavenResolverTask implements Task {
     @Override
     public void execute(BuildContext context) throws Exception {
         //  https://github.com/shrinkwrap/resolver
-        String coordinate = "commons-fileupload:commons-fileupload:1.3.2";
-        File[] files = Maven.resolver().resolve(coordinate).withTransitivity().asFile();
+        //        String coordinate = "commons-fileupload:commons-fileupload:1.3.2";
+        String coordinate = "org.m410.fabricate:garden:0.2-SNAPSHOT";
+
+        File[] files = Maven.configureResolver()
+                .withClassPathResolution(false)
+                .withMavenCentralRepo(false)
+                .withRemoteRepo("m410-snapshot", "http://repo.m410.org/content/repositories/snapshots/", "default")
+                .resolve(coordinate)
+                .withTransitivity()
+                .asFile();
+
         Arrays.stream(files).forEach(f -> System.out.println(f.getAbsolutePath()));
     }
 }
